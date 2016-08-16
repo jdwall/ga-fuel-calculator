bc5f46b7d174a0801686986f7fea750a963afe50
Jacob Wall <jacob.wall@stackpath.com>
2016-08-16 14:20:24 -0500

    Initial Commit

---
+++ ga fuel calculator/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ga fuel calculator
+//
+//  Created by Jacob Wall on 8/16/16.
+//
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
